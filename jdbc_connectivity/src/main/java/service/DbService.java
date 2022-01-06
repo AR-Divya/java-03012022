d8aa70bf066a974e6963aa64e3c2ccad6cfd6ce2
divya.ar <divya.ar@collabera.com>
2022-01-06 12:22:32 +0530

    db connectivity : CURD operations : update

--- jdbc_connectivity/src/main/java/service/DbService.java
+++ jdbc_connectivity/src/main/java/service/DbService.java
@@ -1,10 +1,7 @@
 package service;
 
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DbService {
     private final Connection connection;
@@ -12,7 +9,7 @@ public class DbService {
         this.connection = connection;
     }
 
-    //insert query
+    //insert query - saving data to database
     public int create(int empId, String empNm, Date dob, boolean isManager) throws SQLException{
         String sql = "insert into emp_info values(?, ?, ?, ?)";
         PreparedStatement ps = connection.prepareStatement(sql);
@@ -20,7 +17,7 @@ public class DbService {
         ps.setString(2, empNm);
         ps.setDate(3, dob);
         ps.setBoolean(4, isManager);
-        //above lines create SQL statement
+        //above 5 lines create SQL statement
 
         int affected = ps.executeUpdate(); // actually firing the query
         return affected;
@@ -37,8 +34,20 @@ public class DbService {
         return 0;
     }
 
-    //select query
-    public void find(){
+    //select query - reading the data from database
+    public void find() throws SQLException{
+        String sql = "select * from emp_info";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery(); // logical representation of physical table
+        while (rs.next()){
+            int id = rs.getInt("emp_id");
+            String name = rs.getString("emp_name");
+            Date dob = rs.getDate("dob");
+            boolean isManager = rs.getBoolean("is_manager");
+
+            System.out.println("id : "+id + "name:"+name +"dob:"+dob.toString() +"Manager :"+isManager);
+        }
+        rs.close(); // important to close the result set
 
     }
 }
