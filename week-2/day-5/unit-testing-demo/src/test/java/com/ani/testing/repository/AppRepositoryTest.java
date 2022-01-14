b87255e798b96cd209880b288a510e5cf81a6b8d
divya.ar <divya.ar@collabera.com>
2022-01-14 14:56:08 +0530

    unit testing : repository layer - tests

--- week-2/day-5/unit-testing-demo/src/test/java/com/ani/testing/repository/AppRepositoryTest.java
+++ week-2/day-5/unit-testing-demo/src/test/java/com/ani/testing/repository/AppRepositoryTest.java
@@ -3,11 +3,8 @@ package com.ani.testing.repository;
 import com.ani.testing.domain.App;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.Date;
 import java.time.LocalDate;
